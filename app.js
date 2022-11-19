d7ecee3d459fa163b77d41aeeb91f9909c053459
nedelcucosminadrian <nedelcuadriancosmin@gmail.com>
2022-11-19 17:38:16 +0200

    added

--- app.js
+++ app.js
@@ -20,6 +20,14 @@ let sasiuUpdate = document.querySelector(".updateByVIN");
 let disponibilaUpdate = document.querySelector(".disponibilitateModificare");
 let pretModificare = document.querySelector(".pretModificare");
 
+let inputUpdateByVIN = document.querySelector(".updateByVIN");
+let inputDispModificare = document.querySelector(".disponibilitateModificare");
+let inputPretModificare = document.querySelector(".pretModificare");
+let inputModelModificare = document.querySelector(".modelModificare");
+let inputMarcaModificare = document.querySelector(".marcaModificare");
+let inputAnModificare = document.querySelector(".anModificare");
+let buttonUpdateByVin = document.querySelector(".btn-update-vin");
+
 container.innerHTML = createRows(masini);
 filterMarca.innerHTML = createFilterOptions(getDistinctMarca(masini));
 
@@ -76,5 +84,54 @@ checkBoxAvailableCars.addEventListener("change", (e) => {
 //nu se realizeaza stergerea din cauza functiei deleteCarByVIN
 buttonDeleteVIN.addEventListener("click", (e) => {
   let vinToBeDelete = inputDeleteByVIN.value;
-  container.innerHTML = createRows(deleteCarByVIN(masini, vinToBeDelete));
+  masini = deleteCarByVIN(masini, vinToBeDelete);
+  container.innerHTML = createRows(masini);
+});
+
+buttonUpdateByVin.addEventListener("click", (e) => {
+  let vin = inputUpdateByVIN.value;
+  let disp = inputDispModificare.value;
+  let marca = inputMarcaModificare.value;
+  let model = inputModelModificare.value;
+  let an = inputAnModificare.value;
+  let pret = inputPretModificare.value;
+
+  let dispBool = false;
+  if (disp === "da") {
+    dispBool = true;
+  }
+  masini = updateCarByVin(masini, vin, dispBool, pret, model, marca, an);
+  container.innerHTML = createRows(masini);
+});
+
+container.addEventListener("click", (e) => {
+  let obj = e.target; //imi indica cine a trigaruit eventul
+
+  //type-ul/tag-ul elemetului
+  if (obj.tagName == "BUTTON") {
+    let model = obj.classList[2];
+    masini = deleteCarByModel(masini, model);
+    container.innerHTML = createRows(masini);
+  }
+});
+
+container.addEventListener("click", (e) => {
+  let obj = e.target;
+  if (obj.classList.contains("car")) {
+    let id = obj.classList[1].substr(obj.classList[1].length - 1);
+    console.log(id);
+    masina = getCarById(masini, Number(id));
+    let sasiu = masina.sasiu;
+    inputUpdateByVIN.value = sasiu;
+    inputAnModificare.value = masina.an;
+    inputModelModificare.value = masina.model;
+    inputMarcaModificare.value = masina.marca;
+    inputPretModificare.value = masina.pret;
+
+    if (masina.pret) {
+      inputDispModificare.value = "da";
+    } else {
+      inputDispModificare.value = "nu";
+    }
+  }
 });
